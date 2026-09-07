6a565d6f94bb8648dc005b67ff5f865722299f9a
matheus_neves <mthsnevesdev@outlook.com>
2026-09-07 15:52:19 -0300

    Add: Testes unitários dos métodos Get e Create Transaction

---
+++ Test/PayFlow.Application.Tests/CreateTransactionServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Moq;
+using PayFlow.Application.Dtos;
+using PayFlow.Application.Services;
+using PayFlow.Domain.Entities;
+using PayFlow.Domain.Interface;
+using Xunit;
+
+namespace PayFlow.Application.Tests;
+
+public class CreateTransactionServiceTests
+{
+    [Fact]
+    public async Task CreateTransactionAsync_AmountIsZero_ThrowsArgumentException()
+    {
+        Mock<ITransactionRepository> mock = new Mock<ITransactionRepository>();
+        CreateTransactionService service = new CreateTransactionService(mock.Object);
+        var transactionDto = new TransactionRequestDto();
+        transactionDto.Amount = 0;
+
+        await Assert.ThrowsAsync<ArgumentException>(()
+            => service.CreateTransactionAsync(transactionDto));
+
+        mock.Verify(x => x.AddAsync(It.IsAny<Transaction>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CreateTransactionAsync_ValidRequest_ReturnsTransactionAndSavesIt()
+    {
+        Mock<ITransactionRepository> mock = new Mock<ITransactionRepository>();
+        CreateTransactionService service = new CreateTransactionService(mock.Object);
+        var transactionDto = new TransactionRequestDto();
+        transactionDto.Amount = 1;
+
+        var result = await service.CreateTransactionAsync(transactionDto);
+
+        Assert.NotNull(transactionDto);
+        Assert.Equal(transactionDto.Amount, result.Amount);
+        mock.Verify(x => x.AddAsync(It.IsAny<Transaction>()), Times.Once);
+    }
+}
---
+++ Test/PayFlow.Application.Tests/GetTransactionServiceTests.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using PayFlow.Application.Dtos;
+using PayFlow.Application.Services;
+using PayFlow.Domain.Entities;
+using PayFlow.Domain.Interface;
+using Xunit;
+
+namespace PayFlow.Application.Tests;
+
+public class GetTransactionServiceTests
+{
+    [Fact]
+    public async Task GetById_WhenTransactionExists_ReturnsTransactionResponseDto()
+    {
+        Mock<ITransactionRepository> mock = new Mock<ITransactionRepository>();
+        GetTransactionService service = new GetTransactionService(mock.Object);
+
+        var id = Guid.NewGuid();
+        var transactionFake = new Transaction
+        {
+            Id = id,
+            Amount = 100,
+            UserId = Guid.NewGuid()
+        };
+
+        mock.Setup(x => x.GetByIdAsync(id)).ReturnsAsync(transactionFake);
+        var result = await service.GetById(id);
+
+        Assert.NotNull(result);
+        Assert.Equal(id, result.Id);
+    }
+
+    [Fact]
+    public async Task GetById_WhenTransactionDoesNotExist_ThrowsKeyNotFoundException()
+    {
+        Mock<ITransactionRepository> mock = new Mock<ITransactionRepository>();
+        GetTransactionService service = new GetTransactionService(mock.Object);
+        
+        var id = Guid.NewGuid();
+        
+        mock.Setup(x => x.GetByIdAsync(id)).ReturnsAsync((Transaction?)null);
+    
+        
+        await Assert.ThrowsAsync<KeyNotFoundException>(()
+            => service.GetById(id));
+      
+    }
+}
